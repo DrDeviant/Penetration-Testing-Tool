9c7c1671a29b556f5490a93a24bc57228b0febfe
Phanindra Sai Bollineni <87596277+PhanindraSaiBollineni@users.noreply.github.com>
2021-10-19 14:47:18 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,5 +1,23 @@
-# import pyfiglet module
+import random
+from colorama import Fore, Back, Style
+from extras import printcolor
 import pyfiglet
+
+def intro1():
+    print('\n********************************************************')
   
+    print('*                                                      *')
+    print('*                                                      *')
+    print('********************************************************\n')
+
+def intro2():
 result = pyfiglet.figlet_format("Network Pentration Testing")
 print(result)
+    printcolor('RED', '\n    _____________  _______   ________  ________  ____                 ')                                                         
+    
+    
+                                                                                                                                          
+
+def welcome():
+    introList=[intro2]
+    random.choice(introList)()
